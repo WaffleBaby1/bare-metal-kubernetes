d433294f8dc4902e482ea3b0d8bcc5f675f63c74
WaffleBaby1 <austin.lingenfelter@gmail.com>
2022-06-07 09:58:54 -0500

    fixing error

--- Ansible/ansible-install.sh
+++ Ansible/ansible-install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-sudo apt-get install update
+sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
